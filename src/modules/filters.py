fae7613bd95f9ec6502f30adf476de8b09e07879
Alan Nascimento <eualannascimento@gmail.com>
2023-01-07 11:21:00 -0300

    fix: adjust level filter

--- src/modules/filters.py
+++ src/modules/filters.py
@@ -17,28 +17,44 @@ category_dict = {
 
 
 # level
-level_aprendiz = ['Aprendiz', 'Jovem Aprendiz']
-level_estag = ['Estag', 'Estagio', 'Estágio', 'Estagiario', 'Estagiário']
-level_assistant = ['Assistant', 'Assistente']
-level_trainee = ['Trainee']
-level_jr = ['JR', 'Junior', 'Júnior']
-level_pl = ['PL', 'Pleno', 'Plêno', 'Mid-level', 'Mid Level']
-level_sr = ['SR', 'Senior', 'Sênior']
-level_specialist = ['Specialist', 'Especialista']
-level_lead = ['Lead', 'Lider', 'Líder']
-level_coordinator = ['Coordinator', 'Cordenador']
-level_manager = ['Manager', 'Gerente']
+level_00_banco_de_talentos    = ['Banco de Talentos']
+level_01_aprendiz             = ['Aprendiz', 'Jovem Aprendiz']
+level_02_estag                = ['Estag', 'Estagio', 'Estágio', 'Estagiario', 'Estagiário']
+level_03_auxiliar_ajudante    = ['Auxiliar', 'Ajudante']
+level_04_assistente           = ['Assistant', 'Assistente']
+level_05_tecnico              = ['Tecnico', 'Técnico']
+level_06_trainee              = ['Trainee']
+level_07_jr                   = ['JR', 'Junior', 'Júnior']
+level_08_pl                   = ['PL', 'Pleno', 'Plêno', 'Mid-level', 'Mid Level']
+level_09_sr                   = ['SR', 'Senior', 'Sênior']
+level_10_consultor            = ['Consultant', 'Consultor']
+level_11_specialist           = ['Specialist', 'Especialista']
+level_12_lead                 = ['Lead', 'Lider', 'Líder']
+level_13_supervisor           = ['Supervisor']
+level_14_coordinator          = ['Coordinator', 'Cordenador', 'Coordenadora']
+level_15_manager              = ['Manager', 'Gerente']
+level_16_sr_manager           = ['SR Manager', 'Gerente Senior', 'Gerente Sênior', 'Gerente Departamental']
+level_17_superintendente      = ['Superintendente']
+level_18_diretor              = ['Diretor']
 
 category_level = {
-   "Aprendiz" : level_aprendiz
-   ,"Estágio" : level_estag
-   ,"Assistente": level_assistant
-   ,"Trainee" : level_trainee
-   ,"Junior" : level_jr
-   ,"Pleno" : level_pl
-   ,"Sênior" : level_sr
-   ,"Especialista" : level_specialist
-   ,"Líder" : level_lead
-   ,"Coordenador" : level_coordinator
-   ,"Gerente" : level_manager
+   "00 - Banco de Talentos"         :level_00_banco_de_talentos 
+   ,"01 - Aprendiz"                 :level_01_aprendiz          
+   ,"02 - Estágiario"               :level_02_estag             
+   ,"03 - Auxiliar/Ajudante"        :level_03_auxiliar_ajudante 
+   ,"04 - Assistente"               :level_04_assistente        
+   ,"05 - Técnico"                  :level_05_tecnico           
+   ,"06 - Trainee"                  :level_06_trainee           
+   ,"07 - Júnior"                   :level_07_jr                
+   ,"08 - Pleno"                    :level_08_pl                
+   ,"09 - Sênior"                   :level_09_sr                
+   ,"10 - Consultor"                :level_10_consultor         
+   ,"11 - Especialista"             :level_11_specialist        
+   ,"12 - Líder"                    :level_12_lead              
+   ,"13 - Supervisor"               :level_13_supervisor        
+   ,"14 - Coordenador"              :level_14_coordinator       
+   ,"15 - Gerente"                  :level_15_manager           
+   ,"16 - Gerente Sênior"           :level_16_sr_manager        
+   ,"17 - Superintendente"          :level_17_superintendente   
+   ,"18 - Diretor"                  :level_18_diretor           
 }
